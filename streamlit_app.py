a81c7538e1121827c7922f15cf05336521857fc2
LiselotteVanOmbergen <78793945+LiselotteVanOmbergen@users.noreply.github.com>
2024-05-08 16:09:11 +0200

    toevoeging motivatie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,32 @@
 import streamlit as st
 import os
 import openai
-from langchain_community.llms import GPT4All
+
 from openai import OpenAI
 import os
+from openai import OpenAI
+from langchain_community.document_loaders import PyPDFLoader, DataFrameLoader
+from dotenv import load_dotenv, find_dotenv
+import pandas as pd
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma, FAISS, LanceDB
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import (
+    RunnableParallel,
+    RunnableLambda,
+    RunnablePassthrough
+)
+from langchain_core.output_parsers import StrOutputParser
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
 
 
 openai.api_key = os.getenv(st.secrets["OPENAI_API_KEY"])
 
 
-st.title("Vegan maaltijplangenerator")
+st.title("Vegan maaltijdplangenerator")
 
 
 def generate_meal_plan(gender= 'vrouw', age = 34, height = 163, weight = 75, activity_level = 'gemiddeld', goal = '0.5 kilo per week afvallen'):
@@ -23,6 +40,9 @@ def generate_meal_plan(gender= 'vrouw', age = 34, height = 163, weight = 75, act
         {"role": "user", "content": user_template}
     ]
 )
+        
+
+        
 
    
 
@@ -54,3 +74,83 @@ if st.button('Genereer Maaltijdplan'):
 
 
 
+import random
+def generate_motivation():
+     
+    onderwerpen = [
+        "Gezondheid",
+        "Milieubewustzijn",
+        "Dierenwelzijn",
+        "Duurzaamheid",
+        "Gewichtsbeheersing",
+        "Ethiek",
+        "Voedseldiversiteit",
+        "Klimaatverandering",
+        "Economische rechtvaardigheid"
+]
+
+    willekeurig_onderwerp = random.choice(onderwerpen)
+
+
+
+    #RAG
+    #Data
+    files_paths = []
+    files_paths.append('/RAG_motivatie/vegan_diet2.pdf')
+    files_paths.append('/RAG_motivatie/sustainability.pdf')
+
+    #Document loader
+    documents_motivatie = []
+    for pdf_path in files_paths:
+            loader = PyPDFLoader(pdf_path)
+            documents_motivatie.extend(loader.load())
+
+
+    #Text Splitter
+    documents_benefits = loader.load_and_split()
+
+    #Embeddings
+    embeddings_model = OpenAIEmbeddings(api_key= openai.api_key)
+    # Vectorstores
+    vectorstore_motivatie = LanceDB.from_documents(documents_motivatie, embeddings_model)
+
+
+
+    llm = ChatOpenAI(openai_api_key= openai.api_key, temperature=0)
+    retriever_motivatie = vectorstore_motivatie.as_retriever(search_type="similarity", search_kwargs={"k": 4})
+    retriever_from_llm = MultiQueryRetriever.from_llm(
+    retriever=retriever_motivatie, llm=llm
+)
+
+
+    template = """Je bent een plantaardige voedingscoach. Beantwoord de vraag enkel op basis van de volgende context:
+
+    vraag: {question}
+    context: {context}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+
+
+
+    def format_docs(documents):
+        return "\n\n".join([d.page_content for d in documents])
+
+
+    from langchain_core.runnables import RunnableParallel
+
+    rag_chain_from_pages = (
+        RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
+        |  prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    rag_chain_with_source = RunnableParallel(
+        {"context": retriever_motivatie, "question": RunnablePassthrough()}
+    ).assign(answer=rag_chain_from_pages)
+    antwoord = rag_chain_with_source.invoke(f"Schrijf een hippe, inspirerende en creatieve aanmoediging of tip op basis van één concreet voordeel, ondersteund door middel van concrete statistieken en cijfers uit de gegeven bronnen, van een plantaardig dieet op vlak van {willekeurig_onderwerp} in maximum een of twee zinnen")
+
+    return antwoord['answer']
+
+
+st.sidebar.markdown(generate_motivation())
