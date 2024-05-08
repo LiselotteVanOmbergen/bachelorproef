93b9e26a4d88bc4f8dce0573c256491be58936d7
LiselotteVanOmbergen <78793945+LiselotteVanOmbergen@users.noreply.github.com>
2024-05-08 18:31:45 +0200

    wijziging voordocument loader

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ from langchain_core.output_parsers import StrOutputParser
 #from langchain.retrievers.self_query.base import SelfQueryRetriever
 from langchain.retrievers.multi_query import MultiQueryRetriever
 #from langchain.chains.query_constructor.base import AttributeInfo
+from RAG_motivatie import generate_motivation
 
 
 openai.api_key = os.getenv(st.secrets["OPENAI_API_KEY"])
@@ -73,88 +74,7 @@ if st.button('Genereer Maaltijdplan'):
 
 
 
-import random
-def generate_motivation():
-     
-    onderwerpen = [
-        "Gezondheid",
-        "Milieubewustzijn",
-        "Dierenwelzijn",
-        "Duurzaamheid",
-        "Gewichtsbeheersing",
-        "Ethiek",
-        "Voedseldiversiteit",
-        "Klimaatverandering",
-        "Economische rechtvaardigheid"
-]
 
-    willekeurig_onderwerp = random.choice(onderwerpen)
-
-
-
-    #RAG
-    #Data
-    doc_url = "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10550159/pdf/pone.0291791.pdf"
-
-    loader = OnlinePDFLoader(doc_url)
-    
-    
-    files_paths = []
-    
-    files_paths.append('https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10550159/pdf/pone.0291791.pdf')
-    #files_paths.append('https://github.com/LiselotteVanOmbergen/bachelorproef/RAG_motivatie/sustainability.pdf') 
-    #Document loader
-    #documents_motivatie = []
-    #for pdf_path in files_paths:
-            #loader = PyPDFLoader(pdf_path)
-            #documents_motivatie.extend(loader.load())
-
-
-    #Text Splitter
-    documents_motivatie = loader.load_and_split()
-
-    #Embeddings
-    embeddings_model = OpenAIEmbeddings(api_key= openai.api_key)
-    # Vectorstores
-    vectorstore_motivatie = LanceDB.from_documents(documents_motivatie, embeddings_model)
-
-
-
-    llm = ChatOpenAI(openai_api_key= openai.api_key, temperature=0)
-    retriever_motivatie = vectorstore_motivatie.as_retriever(search_type="similarity", search_kwargs={"k": 4})
-    retriever_motivatie= MultiQueryRetriever.from_llm(
-    retriever=retriever_motivatie, llm=llm
-)
-
-
-    template = """Je bent een plantaardige voedingscoach. Beantwoord de vraag enkel op basis van de volgende context:
-
-    vraag: {question}
-    context: {context}
-    """
-    prompt = ChatPromptTemplate.from_template(template)
-
-
-
-    def format_docs(documents):
-        return "\n\n".join([d.page_content for d in documents])
-
-
-    from langchain_core.runnables import RunnableParallel
-
-    rag_chain_from_pages = (
-        RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
-        |  prompt
-        | llm
-        | StrOutputParser()
-    )
-
-    rag_chain_with_source = RunnableParallel(
-        {"context": retriever_motivatie, "question": RunnablePassthrough()}
-    ).assign(answer=rag_chain_from_pages)
-    antwoord = rag_chain_with_source.invoke(f"Schrijf een hippe, inspirerende en creatieve aanmoediging of tip op basis van één concreet voordeel, ondersteund door middel van concrete statistieken en cijfers uit de gegeven bronnen, van een plantaardig dieet op vlak van {willekeurig_onderwerp} in maximum een of twee zinnen")
-
-    return antwoord['answer']
 
 
 st.sidebar.markdown(generate_motivation())
