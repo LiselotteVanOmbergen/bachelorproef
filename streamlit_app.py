04b3fb091cf34f1740889dda7cfd95b0810ed16d
LiselotteVanOmbergen <78793945+LiselotteVanOmbergen@users.noreply.github.com>
2024-05-08 17:32:51 +0200

    URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,8 +95,8 @@ def generate_motivation():
     #RAG
     #Data
     files_paths = []
-    files_paths.append('https://github.com/LiselotteVanOmbergen/bachelorproef/tree/5aef3c55d707fa97e2426c8e5f7272494619d8b7/RAG_motivatie/vegan_diet2.pdf')
-    files_paths.append('https://github.com/LiselotteVanOmbergen/bachelorproef/tree/5aef3c55d707fa97e2426c8e5f7272494619d8b7/RAG_motivatie/sustainability.pdf') 
+    files_paths.append('https://github.com/LiselotteVanOmbergen/bachelorproef/RAG_motivatie/vegan_diet2.pdf')
+    files_paths.append('https://github.com/LiselotteVanOmbergen/bachelorproef/RAG_motivatie/sustainability.pdf') 
     #Document loader
     documents_motivatie = []
     for pdf_path in files_paths:
