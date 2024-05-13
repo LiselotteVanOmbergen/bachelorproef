35794c013f06ea5fd3e28c8bf92a71dbdb9825a0
LiselotteVanOmbergen <78793945+LiselotteVanOmbergen@users.noreply.github.com>
2024-05-13 17:24:31 +0200

    ch

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,12 @@ st.title("Vegan maaltijdplangenerator")
 col1, col2 = st.columns(2)
 
     # Maaltijdplan genereren en weergeven in de eerste kolom
+with col2:
+        st.subheader('')
+        st.write(generate_motivation())
 with col1:
         
-        gender = st.selectbox('Geslacht', ['Man', 'Vrouw', 'Non-binair persoon'])
+        gender = st.selectbox('Geslacht', ['Vrouw', 'Man', 'Non-binair persoon'])
         age = st.number_input('Leeftijd', min_value=1, max_value=100, value=30, step=1)
         height = st.number_input('Lengte (cm)', min_value=1,
                          max_value=220, value=170, step=1)
@@ -29,20 +32,17 @@ with col1:
                               'Sedentair', 'Licht actief', 'Gemiddeld actief', 'Zeer actief'])
         goal = st.selectbox('Doel', ['0.5 kilo per week aankomen', '1 kilo per week aankomen',
                     '0.5 kilo per week afvallen', '1 kilo per week afvallen', 'Onderhouden'])
-
-        st.subheader('Jouw Maaltijdplan')
         
     # Knop om maaltijdplan te genereren
         if st.button('Genereer Maaltijdplan'):
             st.header('Jouw Maaltijdplan')
-            mealplan =(generate_mealplan(gender= 'vrouw', age = 34, height = 163, weight = 75, activity_level = 'gemiddeld', goal = '0.5 kilo per week afvallen'))
-            st.write(dict_to_text(mealplan))
+            mealplan =(generate_mealplan(gender, age, height , weight, activity_level, goal))
+            st.write(mealplan)
+            #st.write(dict_to_text(mealplan))
             # Genereer maaltijdplan op basis van gebruikersinvoer
             st.header('Boodschappenlijst')
             st.write(print_shopping_list(generate_shopping_list_dict(mealplan)))
     # Motivatie genereren en weergeven in de tweede kolom
-with col2:
-        st.subheader('')
-        st.write(generate_motivation())
+
         
    
