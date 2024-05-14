dc12ae0e775e1fa1a361a7056559db6e896822c4
LiselotteVanOmbergen <78793945+LiselotteVanOmbergen@users.noreply.github.com>
2024-05-14 16:08:37 +0200

    correctie

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,16 +51,15 @@ with col2:
 
 
 if st.button('Genereer maaltijdplan'):
-            dietary_requirements = generate_dietary_requirements(gender = gender, age = age, height = height, weight = weight, activity_level = activity_level, goal = goal)
             st.header('Jouw maaltijdplan')
-            mealplan = generate_mealplan(generate_dietary_requirements(gender, age, height, weight,  activity_level, goal, user_requirements)
-            #st.text(dict_to_text(json.loads(mealplan)))
-            #st.download_button("Download maaltijdplan", mealplan)
+            mealplan = generate_mealplan(generate_dietary_requirements(gender, age, height, weight,  activity_level, goal), user_requirements)
+            st.text(dict_to_text(json.loads(mealplan)))
+            st.download_button("Download maaltijdplan", mealplan)
             st.write(mealplan)
 
             st.header('Boodschappenlijst')
        
             boodschappenlijst = dict_to_text(generate_shopping_list_dict(json.loads(mealplan)))
             st.text(boodschappenlijst)
-            #st.download_button("Download boodschappenlijst", boodschappenlijst)
+            st.download_button("Download boodschappenlijst", boodschappenlijst)
             st.write(boodschappenlijst)
