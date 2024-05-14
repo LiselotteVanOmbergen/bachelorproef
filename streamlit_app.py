ee08fd33682d3045b03b085e9a7b4076e50e750e
LiselotteVanOmbergen <78793945+LiselotteVanOmbergen@users.noreply.github.com>
2024-05-14 12:12:16 +0200

    layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,38 +14,27 @@ st.title(":seedling: Vegan maaltijdplangenerator :seedling:")
 
 #col1, col2 = st.columns(2)
 
-if 'mealplan_generated' not in st.session_state:
-    st.session_state.mealplan_generated = False
 
-if not st.session_state.mealplan_generated:
   
-        st.write(generate_motivation())
+st.write(generate_motivation())
 
-        if st.button('Genereer maaltijdplan'):
-            st.session_state.mealplan_generated = True
+if st.button('Genereer maaltijdplan'):
+    gender = st.selectbox('Geslacht', ['Vrouw', 'Man', 'Non-binair persoon'])
+    age = st.number_input('Leeftijd', min_value=1, max_value=100, value=30, step=1)
+    height = st.number_input('Lengte (cm)', min_value=1,
+                            max_value=220, value=170, step=1)
+    weight = st.number_input('Gewicht (kg)', min_value=1,
+                            max_value=500, value=70, step=1)
+    activity_level = st.selectbox('Activiteitsniveau', [
+        'Sedentair', 'Licht actief', 'Gemiddeld actief', 'Zeer actief'])
+    goal = st.selectbox('Doel', ['0.5 kilo per week aankomen', '1 kilo per week aankomen',
+                                '0.5 kilo per week afvallen', '1 kilo per week afvallen', 'Onderhouden'])
 
-if st.session_state.mealplan_generated:
+
   
-        gender = st.selectbox('Geslacht', ['Vrouw', 'Man', 'Non-binair persoon'])
-        age = st.number_input('Leeftijd', min_value=1, max_value=100, value=30, step=1)
-        height = st.number_input('Lengte (cm)', min_value=1, max_value=220, value=170, step=1)
-        weight = st.number_input('Gewicht (kg)', min_value=1, max_value=500, value=70, step=1)
-        activity_level = st.selectbox('Activiteitsniveau', ['Sedentair', 'Licht actief', 'Gemiddeld actief', 'Zeer actief'])
-        goal = st.selectbox('Doel', ['0.5 kilo per week aankomen', '1 kilo per week aankomen',
-                                     '0.5 kilo per week afvallen', '1 kilo per week afvallen', 'Onderhouden'])
-if 'data_sent' not in st.session_state:
-    st.session_state.data_sent = False
 
-if not st.session_state.data_sent:
-  
-        st.subheader(':earth_africa:')
-       
-
-        if st.button('Genereer maaltijdplan'):
-            st.session_state.data_sent = True
-
-if st.session_state.data_sent:
-        if st.button('Indienen'):
+   
+if st.button('Indienen'):
             st.header('Jouw maaltijdplan')
             mealplan = generate_mealplan(gender, age, height, weight, activity_level, goal)
             st.text(dict_to_text(json.loads(mealplan)))
