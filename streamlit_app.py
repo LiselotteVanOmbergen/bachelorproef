1ec361f57c096b4d16332c6d2e6a7aa4ae3d79b4
LiselotteVanOmbergen <78793945+LiselotteVanOmbergen@users.noreply.github.com>
2024-05-16 12:02:37 +0200

    sesstion state

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,52 +13,65 @@ openai.api_key = os.getenv("OPENAI_API_KEY", st.secrets.get("OPENAI_API_KEY"))
 
 st.title(":seedling: Vegan maaltijdplangenerator :seedling:")
 
-col1, col2 = st.columns(2)
+if 'form_submitted' not in st.session_state:
+    st.session_state.form_submitted = False
+
+if 'user_inputs' not in st.session_state:
+    st.session_state.user_inputs = {
+        'gender': None,
+        'age': 30,
+        'height': 170,
+        'weight': 70,
+        'activity_level': 'Gemiddeld actief',
+        'goal': 'Onderhouden',
+        'ingredient_ontbijt': '',
+        'ingredient_lunch': '',
+        'ingredient_diner': ''
+    }
 
 
-  
-with st.form(key='user_input_form'):
-    with col1:
-        st.write(generate_motivation())
+if not st.session_state.form_submitted:
+    with st.form(key='user_input_form'):
+        col1, col2 = st.columns(2)
 
-        gender = st.selectbox('Geslacht', ['Vrouw', 'Man', 'Non-binair persoon'])
-        age = st.number_input('Leeftijd', min_value=1,
-                        max_value=100, value=30, step=1)
-        height = st.number_input('Lengte (cm)', min_value=1,
-                            max_value=220, value=170, step=1)
-        weight = st.number_input('Gewicht (kg)', min_value=1,
-                            max_value=500, value=70, step=1)
-        activity_level = st.selectbox('Activiteitsniveau', [
-        'Sedentair', 'Licht actief', 'Gemiddeld actief', 'Zeer actief'])
-        goal = st.selectbox('Doel', ['0.5 kilo per week aankomen', '1 kilo per week aankomen',
-                                '0.5 kilo per week afvallen', '1 kilo per week afvallen', 'Onderhouden'])
+        with col1:
+            st.write(generate_motivation())
 
-# Invoervelden voor ontbijt
-with col2:
-    st.subheader("Ontbijt")
-    ingredient_ontbijt = st.text_input("Ingrediënt of gerecht voor ontbijt")
-    # Voeg meer ingrediëntenvelden toe zoals hierboven indien nodig voor ontbijt
+            st.session_state.user_inputs['gender'] = st.selectbox('Geslacht', ['Vrouw', 'Man', 'Non-binair persoon'], index=st.session_state.user_inputs['gender'])
+            st.session_state.user_inputs['age'] = st.number_input('Leeftijd', min_value=1, max_value=100, value=st.session_state.user_inputs['age'], step=1)
+            st.session_state.user_inputs['height'] = st.number_input('Lengte (cm)', min_value=1, max_value=220, value=st.session_state.user_inputs['height'], step=1)
+            st.session_state.user_inputs['weight'] = st.number_input('Gewicht (kg)', min_value=1, max_value=500, value=st.session_state.user_inputs['weight'], step=1)
+            st.session_state.user_inputs['activity_level'] = st.selectbox('Activiteitsniveau', ['Sedentair', 'Licht actief', 'Gemiddeld actief', 'Zeer actief'], index=['Sedentair', 'Licht actief', 'Gemiddeld actief', 'Zeer actief'].index(st.session_state.user_inputs['activity_level']))
+            st.session_state.user_inputs['goal'] = st.selectbox('Doel', ['0.5 kilo per week aankomen', '1 kilo per week aankomen', '0.5 kilo per week afvallen', '1 kilo per week afvallen', 'Onderhouden'], index=['0.5 kilo per week aankomen', '1 kilo per week aankomen', '0.5 kilo per week afvallen', '1 kilo per week afvallen', 'Onderhouden'].index(st.session_state.user_inputs['goal']))
 
-    # Invoervelden voor lunch
-    st.subheader("Lunch")
-    ingredient_lunch = st.text_input("Ingrediënt of gerecht voor lunch")
-    # Voeg meer ingrediëntenvelden toe zoals hierboven indien nodig voor lunch
-
-    # Invoervelden voor diner
-    st.subheader("Diner")
-    ingredient_diner = st.text_input("Ingrediënt of gerecht voor diner")
-    user_requirements = f"{ingredient_ontbijt} voor ontbijt, {ingredient_lunch} voor lunch en {ingredient_diner} voor diner"
-
-
-if st.button('Genereer maaltijdplan'):
-            st.header('	:carrot: Jouw maaltijdplan')
-            mealplan = generate_mealplan(generate_dietary_requirements(gender, age, height, weight,  activity_level, goal), user_requirements)
-            st.text(dict_to_text(json.loads(mealplan)))
-            st.download_button("Download maaltijdplan", mealplan)
+        
+        with col2:
+            st.subheader("Ontbijt")
+            st.session_state.user_inputs['ingredient_ontbijt'] = st.text_input("Ingrediënt of gerecht voor ontbijt", value=st.session_state.user_inputs['ingredient_ontbijt'])
+            
+            st.subheader("Lunch")
+            st.session_state.user_inputs['ingredient_lunch'] = st.text_input("Ingrediënt of gerecht voor lunch", value=st.session_state.user_inputs['ingredient_lunch'])
             
 
-            st.header('	:shopping_trolley: Boodschappenlijst')
-            boodschappenlijst = dict_to_text(generate_shopping_list_dict(json.loads(mealplan)))
-            st.text(boodschappenlijst)
-            st.download_button("Download boodschappenlijst", boodschappenlijst)
-            
+            st.subheader("Diner")
+            st.session_state.user_inputs['ingredient_diner'] = st.text_input("Ingrediënt of gerecht voor diner", value=st.session_state.user_inputs['ingredient_diner'])
+
+        
+        submitted = st.form_submit_button('Genereer maaltijdplan')
+
+    
+    if submitted:
+        st.session_state.form_submitted = True
+
+if st.session_state.form_submitted:
+    user_requirements = f"{st.session_state.user_inputs['ingredient_ontbijt']} voor ontbijt, {st.session_state.user_inputs['ingredient_lunch']} voor lunch en {st.session_state.user_inputs['ingredient_diner']} voor diner"
+
+    st.header(' :carrot: Jouw maaltijdplan')
+    mealplan = generate_mealplan(generate_dietary_requirements(st.session_state.user_inputs['gender'], st.session_state.user_inputs['age'], st.session_state.user_inputs['height'], st.session_state.user_inputs['weight'],  st.session_state.user_inputs['activity_level'], st.session_state.user_inputs['goal']), user_requirements)
+    st.text(dict_to_text(json.loads(mealplan)))
+    st.download_button("Download maaltijdplan", mealplan)
+    
+    st.header(' :shopping_trolley: Boodschappenlijst')
+    boodschappenlijst = dict_to_text(generate_shopping_list_dict(json.loads(mealplan)))
+    st.text(boodschappenlijst)
+    st.download_button("Download boodschappenlijst", boodschappenlijst)
