f16324dd352fdb01a065e2b1acf5a496e0b1e9d5
LiselotteVanOmbergen <78793945+LiselotteVanOmbergen@users.noreply.github.com>
2024-05-16 17:09:21 +0200

    opnieuw indienen

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,43 +11,29 @@ from shopping_list import generate_shopping_list_dict
 st.set_page_config(layout="wide")
 openai.api_key = os.getenv("OPENAI_API_KEY", st.secrets.get("OPENAI_API_KEY"))
 
-st.title(":seedling: Veg:green[AI]n maaltijdplangenerator :seedling:") 
+st.title(":seedling: Veg:green[AI]n maaltijdplangenerator :seedling:")
 
 cola, colb = st.columns(2)
 
 if 'motivation_content' not in st.session_state:
     st.session_state.motivation_content = generate_motivation()
 
-import streamlit as st
-
 with cola.container(height=200):
     st.write(st.session_state.motivation_content)
-    
+
 with colb.container(height=200):
     st.markdown('<span style="color:green">Vergeet niet om dagelijks een vitamine B12-supplement in te nemen, aangezien deze vitamine van nature alleen voorkomt in dierlijke producten. Naast vitamine B12, kunnen ook andere supplementen worden overwogen om ervoor te zorgen dat je alle essentiële voedingsstoffen binnenkrijgt. Denk hierbij aan vitamine D, omega-3 vetzuren, calcium en ijzer.</span>', unsafe_allow_html=True)
 
 if 'form_submitted' not in st.session_state:
     st.session_state.form_submitted = False
 
-if 'user_inputs' not in st.session_state:
-    st.session_state.user_inputs = {
-        'gender': None,
-        'age': 30,
-        'height': 170,
-        'weight': 70,
-        'activity_level': 'Gemiddeld actief',
-        'goal': 'Onderhouden',
-        'ingredient_ontbijt': '',
-        'ingredient_lunch': '',
-        'ingredient_diner': '',
-        'ingredient_snack':'',
-        'ingredient_dessert':''
-    }
+if 'mealplan_generated' not in st.session_state:
+    st.session_state.mealplan_generated = False
 
-if not st.session_state.form_submitted:
+if not st.session_state.form_submitted or st.session_state.mealplan_generated:
     with st.form(key='user_input_form'):
         submitted = False
-        col1, col2 = st.columns(2)    
+        col1, col2 = st.columns(2)
 
         with col1:
             st.write("Vul hieronder je persoonlijke gegevens in.")
@@ -58,15 +44,15 @@ if not st.session_state.form_submitted:
             st.session_state.user_inputs['activity_level'] = st.selectbox('Activiteitsniveau', ['Sedentair', 'Licht actief', 'Gemiddeld actief', 'Zeer actief'], index=['Sedentair', 'Licht actief', 'Gemiddeld actief', 'Zeer actief'].index(st.session_state.user_inputs['activity_level']))
             st.session_state.user_inputs['goal'] = st.selectbox('Doel', ['0.5 kilo per week aankomen', '1 kilo per week aankomen', '0.5 kilo per week afvallen', '1 kilo per week afvallen', 'Onderhouden'], index=['0.5 kilo per week aankomen', '1 kilo per week aankomen', '0.5 kilo per week afvallen', '1 kilo per week afvallen', 'Onderhouden'].index(st.session_state.user_inputs['goal']))
 
-        
+
         with col2:
             st.write("Vul hieronder specifiek gewenste ingrediënten of gerechten in voor een bepaalde maaltijd. Dit is optioneel: je kan dit ook oningevuld laten of slechts gedeeltelijk invullen.")
             st.subheader(":pancakes: Ontbijt")
             st.session_state.user_inputs['ingredient_ontbijt'] = st.text_input("Ingrediënt of gerecht voor ontbijt", value=st.session_state.user_inputs['ingredient_ontbijt'])
-            
+
             st.subheader(":sandwich: Lunch")
             st.session_state.user_inputs['ingredient_lunch'] = st.text_input("Ingrediënt of gerecht voor lunch", value=st.session_state.user_inputs['ingredient_lunch'])
-            
+
 
             st.subheader(":spaghetti: Diner")
             st.session_state.user_inputs['ingredient_diner'] = st.text_input("Ingrediënt of gerecht voor diner", value=st.session_state.user_inputs['ingredient_diner'])
@@ -76,18 +62,17 @@ if not st.session_state.form_submitted:
 
             st.subheader(":ice_cream: Dessert")
             st.session_state.user_inputs['ingredient_dessert'] = st.text_input("Ingrediënt of gerecht voor dessert", value=st.session_state.user_inputs['ingredient_dessert'])
-        
+
             submitted = st.form_submit_button('Genereer maaltijdplan')
 
     if submitted:
         st.session_state.form_submitted = True
-        if st.button("Nieuw maaltijdplan"):
-            st.session_state.form_submitted = False
+        st.session_state.mealplan_generated = True
 
-if st.session_state.form_submitted:
+if st.session_state.mealplan_generated:
     user_requirements = f"{st.session_state.user_inputs['ingredient_ontbijt']} voor ontbijt, {st.session_state.user_inputs['ingredient_lunch']} voor lunch, {st.session_state.user_inputs['ingredient_diner']} voor diner, {st.session_state.user_inputs['ingredient_snack']} voor snack en {st.session_state.user_inputs['ingredient_dessert']} voor dessert"
     col1, col2 = st.columns([0.7, 0.3])
-    
+
     with col1:
         st.header(' :carrot: Jouw maaltijdplan')
         mealplan = generate_mealplan(generate_dietary_requirements(st.session_state.user_inputs['gender'], st.session_state.user_inputs['age'], st.session_state.user_inputs[
@@ -101,5 +86,6 @@ if st.session_state.form_submitted:
         st.text(boodschappenlijst)
         st.download_button("Download boodschappenlijst", boodschappenlijst)
 
-if st.button("Nieuw maaltijdplan"):
-        st.session_state.form_submitted = False
+        if st.button("Nieuw maaltijdplan"):
+            st.session_state.form_submitted = False
+            st.session_state.mealplan_generated = False
